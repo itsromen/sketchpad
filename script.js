5cef489fe0f93cc66da6fac18303bea0ca8812a3
itsromen <ugtromen449@gmail.com>
2025-06-01 09:54:23 +0300

    Add event listeners for drawing

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-for (i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    const sec = document.querySelector("section");
-    sec.appendChild(div);
+for (i = 0; i < 256; i++) {
+  const div = document.createElement("div");
+  const sec = document.querySelector("section");
+  sec.appendChild(div);
 }
+
+const sec = document.querySelector("section");
+let isDrawing = false;
+
+sec.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  e.target.style.backgroundColor = "red";
+});
+
+sec.addEventListener("mouseover", (e) => {
+  if (isDrawing) {
+    e.target.style.backgroundColor = "red";
+  }
+});
+
+sec.addEventListener("mouseup", () => {
+    isDrawing = false;
+})
